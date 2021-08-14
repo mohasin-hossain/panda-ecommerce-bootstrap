124541f4282b2ead25dd29cbd2ccffcabedba54d
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2021-08-14 17:34:51 +0600

    Added border to all cards through js

--- js/script.js
+++ js/script.js
@@ -4,4 +4,9 @@ for (const h2 of h2Collection) {
   h2.style.color = "coral";
 }
 
+// Adding border radius to all the cards
+const cards = document.getElementsByClassName("card");
+for (const card of cards) {
+  card.style.borderRadius = "10px";
+}
 
