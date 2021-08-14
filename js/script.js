9aad2a82fb7f63e63bc484b0212876bc01656810
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2021-08-14 17:45:24 +0600

    Added to cart functionality through js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,13 @@ for (const card of cards) {
   card.style.borderRadius = "10px";
 }
 
+// Adding Eventlistener to buy now button and showing added to cart
+const buyNow = document.getElementsByClassName("btn-buy-now");
+for (const button of buyNow) {
+  button.addEventListener("click", function () {
+    button.innerText = "Added to cart";
+    button.setAttribute("disabled", true);
+    button.style.backgroundImage =
+      "linear-gradient(to bottom, #f882b1 0%, #fa79ab 100%)";
+  });
+}
